c867d09aaecd4fb315c7a643b45b6e598cbca917
dani2c <da.cuevasc@duocuc.cl>
2024-09-20 21:49:25 -0300

    primer-commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { IonicModule } from '@ionic/angular';
+@NgModule({
+  declarations: [EncabezadoComponent],
+  imports: [
+    CommonModule,
+    IonicModule,
+  ],
+  exports: [EncabezadoComponent] 
+})
+export class SharedModule { }
